e770f2ddd98d985d05166dbfc7717d1ab13b40ce
Your Name <you@example.com>
2026-06-25 13:36:39 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/all";
-import Showcase from "./components/Showcase.jsx";
 import Performance from "./components/Performance.jsx";
 import Features from "./components/Features.jsx";
 import Highlights from "./components/Highlights.jsx";
 import Footer from "./components/Footer.jsx";
+import Showcase from "./components/Showсase.jsx";
 
 gsap.registerPlugin(ScrollTrigger)
 
